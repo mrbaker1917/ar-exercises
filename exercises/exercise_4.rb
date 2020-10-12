2a3bb226bcaa87155c9c6bf213e6cded70def5b1
Mark Baker <mrbaker1917@gmail.com>
2020-10-12 16:41:21 -0700

    adds exercise_4

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,18 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+s1 = Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+s1 = Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+s1 = Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+
+@mens_stores = Store.where(mens_apparel: true)
+
+@mens_stores.each do |store|
+  puts "Store name: #{store.name}, Annual Revenue: #{store.annual_revenue} "
+end
+
+@womens_stores = Store.where('annual_revenue < ?', 1000000)
+@womens_stores = @womens_stores.where(womens_apparel: true)
+@womens_stores.each do |store|
+  puts "Store name: #{store.name}, Annual Revenue: #{store.annual_revenue}"
+end
